25449844de18cae9a2d296fd4d6908965c67e930
陶小江 <tao.xj@hgicreate.com>
2017-11-04 07:49:19 +0000

    TASK #4306 表单提交功能增强

--- src/main/java/com/hgicreate/rno/web/rest/LteNcellRelationResource.java
+++ src/main/java/com/hgicreate/rno/web/rest/LteNcellRelationResource.java
@@ -1,9 +1,6 @@
 package com.hgicreate.rno.web.rest;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
