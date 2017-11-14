3b1b394e117d9db8a5567fb1992dca9f071c0141
伍洪斌 <wu.hb@hgicreate.com>
2017-11-14 10:32:32 +0000

    #1918 移动通信4G网优软件平台功能增强

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,33 +1,11 @@
-// 设置HTML5本地存储中 SelectedMenu 的值为空
+// 设置 HTML5 本地存储中菜单项选择的值为空，这样从登录页进系统就会显示默认首页
 localStorage['SelectedMenuId'] = "";
 localStorage['SelectedMenuFlag'] = "";
 localStorage['SelectedMenuRank'] = "";
 
 $(function () {
-    //幻灯片
+    // 幻灯片
     $(window).load(function () {
-        $(".login-slide").flexslider();
-    });
-
-     //弹出Email后缀名列表
-    $(".email-text").click(function () {
-        $(".email-list").toggle();
-    });
-    $(".email-extensions").mouseleave(function () {
-        $(".email-list").hide();
-    });
-
-    var $emailList = $(".email-list li");
-    //显示Email后缀名
-    $emailList.click(function () {
-        $(".email-name").text($(this).text());
-        $(".email-list").hide();
-    });
-
-    //鼠标经过改变颜色
-    $emailList.hover(function () {
-        $(this).addClass("hover")
-    }, function () {
-        $(this).removeClass("hover")
+        $(".flexslider").flexslider();
     });
 });
