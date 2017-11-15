670d340a111b33d54e182e4265512fa5888720c7
陶小江 <tao.xj@hgicreate.com>
2017-11-15 03:17:21 +0000

    TASK #4306 表单提交功能增强

--- src/main/java/com/hgicreate/rno/service/dto/LteNcellImportFileDTO.java
+++ src/main/java/com/hgicreate/rno/service/dto/LteNcellImportFileDTO.java
@@ -7,12 +7,12 @@ import java.util.Date;
 @Data
 public class LteNcellImportFileDTO {
     private Long id;
-    private String uploadTime;
+    private Date uploadTime;
     private String areaName;
     private String filename;
     private String fileSize;
-    private String startTime;
-    private String completeTime;
+    private Date startTime;
+    private Date completeTime;
     private String createdUser;
     private String status;
 }
--- src/main/webapp/rno4g/lte-ncell-relation/js/lte-ncell-relation.js
+++ src/main/webapp/rno4g/lte-ncell-relation/js/lte-ncell-relation.js
@@ -173,10 +173,17 @@ function showNcellImportResult(data) {
         ],
         "columnDefs": [{
             "render": function(data, type, row) {
+                var d = new Date(parseInt(row['uploadTime'])).Format("yyyy-MM-dd hh:mm:ss");
+                return d;
+            },
+            "targets": 1,
+            "data": null
+        },{
+            "render": function(data, type, row) {
                 if(row['startTime']==""||row['startTime']==null){
                     return "---";
                 }else{
-                    return row['startTime'];
+                    return new Date(parseInt(row['startTime'])).Format("yyyy-MM-dd hh:mm:ss");
                 }
             },
             "targets": 4,
@@ -186,7 +193,7 @@ function showNcellImportResult(data) {
                 if(row['completeTime']==""||row['completeTime']==null){
                     return "---";
                 }else{
-                    return row['completeTime'];
+                    return new Date(parseInt(row['completeTime'])).Format("yyyy-MM-dd hh:mm:ss");
                 }
             },
             "targets": 5,
@@ -298,4 +305,34 @@ function showImportDetail() {
         });
     $("#reportDiv").css("display", "block");
     $("#listInfoDiv").css("display", "none");
+}
+
+/**
+ * 对Date的扩展，将 Date 转化为指定格式的String
+ *月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符，
+ *年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)
+ *例子：
+ *(new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423
+ *(new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18
+ * @title
+ * @param fmt
+ * @returns
+ */
+Date.prototype.Format = function(fmt)
+{
+    var o = {
+        "M+" : this.getMonth()+1,                 //月份
+        "d+" : this.getDate(),                    //日
+        "h+" : this.getHours(),                   //小时
+        "m+" : this.getMinutes(),                 //分
+        "s+" : this.getSeconds(),                 //秒
+        "q+" : Math.floor((this.getMonth()+3)/3), //季度
+        "S"  : this.getMilliseconds()             //毫秒
+    };
+    if(/(y+)/.test(fmt))
+        fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+    for(var k in o)
+        if(new RegExp("("+ k +")").test(fmt))
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+    return fmt;
 }
