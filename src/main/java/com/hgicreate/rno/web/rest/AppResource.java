e6ea9812b0c1e40353b8d89998159097a6fd25a2
伍洪斌 <wu.hb@hgicreate.com>
2017-11-18 08:42:13 +0000

    #1918 移动通信4G网优软件平台功能增强

--- src/main/java/com/hgicreate/rno/web/rest/AppResource.java
+++ src/main/java/com/hgicreate/rno/web/rest/AppResource.java
@@ -2,14 +2,11 @@ package com.hgicreate.rno.web.rest;
 
 import com.hgicreate.rno.domain.App;
 import com.hgicreate.rno.repository.AppRepository;
-import com.hgicreate.rno.service.dto.AreaDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @Slf4j
 @RestController
