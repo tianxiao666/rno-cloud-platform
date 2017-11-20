3ad1c1d7c5c3d02ea3398c501737cee1d985f77c
伍洪斌 <wu.hb@hgicreate.com>
2017-11-20 02:29:09 +0000

    #1918 移动通信4G网优软件平台功能增强

--- src/main/java/com/hgicreate/rno/util/FtpUtils.java
+++ src/main/java/com/hgicreate/rno/util/FtpUtils.java
@@ -12,7 +12,7 @@ public final class FtpUtils {
      * @param moduleName 所属模块名
      * @param fullPath 文件在本地的全路径
      * @param deleteAfterSent 发送完成后是否删除本地文件
-     * @return 文件在FTP上全路径
+     * @return 文件在FTP上的全路径
      */
     public static String sendToFtp(String moduleName, String fullPath, Boolean deleteAfterSent) {
         return null;
