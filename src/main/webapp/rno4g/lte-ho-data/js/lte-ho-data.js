b73812eda4525abd36ac430ec5095aca621f6ba5
陶小江 <tao.xj@hgicreate.com>
2017-11-20 09:14:50 +0000

    TASK #4357 HO切换数据管理功能增强

--- src/main/webapp/rno4g/lte-ho-data/js/lte-ho-data.js
+++ src/main/webapp/rno4g/lte-ho-data/js/lte-ho-data.js
@@ -163,7 +163,7 @@ function showNcellImportDtResult(data) {
     $(".loading").css("display", "none");
     if (data == '') {
         $("#info").css("background", "red");
-        showInfoInAndOut('info', '没有符合条件的Ho切换数据记录');
+        showInfoInAndOut('info', '没有符合条件的切换数据记录');
     }
 
     $('#queryDataResultDT').css("line-height", "12px")
