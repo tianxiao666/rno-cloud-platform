40412f513dba25d1bccacf35c8a2b7013786d8e8
伍洪斌 <wu.hb@hgicreate.com>
2017-11-20 02:26:55 +0000

    STORY #1907 网优云服务平台功能重构

---
+++ src/main/java/com/hgicreate/rno/util/FtpUtils.java
@@ -0,0 +1,21 @@
+package com.hgicreate.rno.util;
+
+/**
+ * FTP 工具类
+ */
+public final class FtpUtils {
+    private FtpUtils() {
+    }
+
+    /**
+     * 把文件发送到FTP服务器
+     * @param moduleName 所属模块名
+     * @param fullPath 文件在本地的全路径
+     * @param deleteAfterSent 发送完成后是否删除本地文件
+     * @return 文件在FTP上全路径
+     */
+    public static String sendToFtp(String moduleName, String fullPath, Boolean deleteAfterSent) {
+        return null;
+    }
+
+}
