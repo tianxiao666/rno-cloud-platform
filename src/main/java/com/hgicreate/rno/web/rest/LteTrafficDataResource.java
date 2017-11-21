740f252e25fcbfb3998236f06662229b17ddc8a3
曾德火 <zeng.dh1@hgicreate.com>
2017-11-21 06:12:19 +0000

    TASK #4360 网络统计数据管理前端显示与后端数据处理开发

--- src/main/java/com/hgicreate/rno/web/rest/LteTrafficDataResource.java
+++ src/main/java/com/hgicreate/rno/web/rest/LteTrafficDataResource.java
@@ -159,7 +159,7 @@ public class LteTrafficDataResource {
             area.setId(Long.parseLong(vm.getAreaId()));
             dataJob.setArea(area);
             dataJob.setCreatedDate(new Date());
-            dataJob.setPriority(2);
+            dataJob.setPriority(1);
             dataJob.setCreatedUser(SecurityUtils.getCurrentUserLogin());
             dataJob.setStatus("等待处理");
             dataJob.setDataStoreType("FTP");
