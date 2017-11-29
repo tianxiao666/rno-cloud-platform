f368597936845197921cc955c66b9f87a7db0871
伍洪斌 <wu.hb@hgicreate.com>
2017-11-29 09:35:27 +0000

    TASK #4401 添加基于 Keycloak 的认证功能

--- src/main/java/com/hgicreate/rno/service/KeycloakAdminCliService.java
+++ src/main/java/com/hgicreate/rno/service/KeycloakAdminCliService.java
@@ -2,9 +2,13 @@ package com.hgicreate.rno.service;
 
 import com.hgicreate.rno.config.Constants;
 import com.hgicreate.rno.domain.User;
+import com.hgicreate.rno.security.SecurityUtils;
+import org.apache.catalina.security.SecurityUtil;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.admin.client.resource.UsersResource;
+import org.keycloak.representations.idm.CredentialRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -36,7 +40,7 @@ public class KeycloakAdminCliService {
      * @param user 用户对象
      * @return 成功返回true，失败返回false
      */
-    public boolean updateAccount(final User user) {
+    boolean updateAccount(final User user) {
         UsersResource usersResource = getInstance().realm(REALM).users();
         List<UserRepresentation> userRepresentationList = usersResource.search(user.getUsername());
         if (userRepresentationList.size() > 0) {
@@ -48,5 +52,18 @@ public class KeycloakAdminCliService {
         } else {
             return false;
         }
+    }
+
+    /**
+     * 重置密码
+     */
+    void resetPassword(String newPassword) {
+        // 设置新密码
+        CredentialRepresentation newCredential = new CredentialRepresentation();
+        UserResource userResource = getInstance().realm(REALM).users().get(SecurityUtils.getAccessToken().getId());
+        newCredential.setType(CredentialRepresentation.PASSWORD);
+        newCredential.setValue(newPassword);
+        newCredential.setTemporary(false);
+        userResource.resetPassword(newCredential);
     }
 }
