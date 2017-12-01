efbff64761380d54b46661bdcbdd2d0cd9e0e0a8
伍洪斌 <wu.hb@hgicreate.com>
2017-12-01 03:31:55 +0000

    TASK #4401 添加基于 Keycloak 的认证功能

--- src/main/java/com/hgicreate/rno/config/Constants.java
+++ src/main/java/com/hgicreate/rno/config/Constants.java
@@ -5,6 +5,9 @@ package com.hgicreate.rno.config;
  */
 public final class Constants {
 
+    /**
+     * 匿名用户，用于关闭认证功能时的缺省用户
+     */
     public static final String ANONYMOUS_USER = "anonymous";
     public static final String ANONYMOUS_NAME = "网优用户";
 
@@ -14,11 +17,10 @@ public final class Constants {
     public static final Integer WEAK_COVERAGE_RSRP_THRESHOLD = -110;
     public static final Integer WEAK_COVERAGE_RS_SINR_THRESHOLD = -3;
 
-    public static final String ADMIN_CONSOLE_CLIENT_ID = "security-admin-console";
     public static final String ADMIN_CLI_CLIENT_ID = "admin-cli";
 
     /**
-     * 缺省地区：天河区
+     * 缺省地区：广州市天河区
      */
     public static final Long DEFAULT_AREA = 440106L;
 
