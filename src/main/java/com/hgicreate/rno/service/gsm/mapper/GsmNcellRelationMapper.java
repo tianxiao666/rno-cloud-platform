32c401bddcde982f4f170eaf91eb5c608e805301
陶小江 <tao.xj@hgicreate.com>
2017-12-06 10:21:50 +0000

    TASK #4431 数据配置管理-GSM邻区数据管理功能增强 邻区文件导入、导入记录查询、数据记录查询及邻区关系查询功能

---
+++ src/main/java/com/hgicreate/rno/service/gsm/mapper/GsmNcellRelationMapper.java
@@ -0,0 +1,19 @@
+package com.hgicreate.rno.service.gsm.mapper;
+
+import com.hgicreate.rno.domain.LteNcellRelation;
+import com.hgicreate.rno.domain.gsm.GsmNcell;
+import com.hgicreate.rno.domain.gsm.GsmNcellRelation;
+import com.hgicreate.rno.service.dto.LteNcellRelationDTO;
+import com.hgicreate.rno.service.gsm.dto.GsmNcellRelationDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface GsmNcellRelationMapper {
+
+    GsmNcellRelationMapper INSTANCE = Mappers.getMapper( GsmNcellRelationMapper.class );
+
+    @Mapping(source = "id", target = "id")
+    GsmNcellRelationDTO ncellToNcellDTO(GsmNcell gsmNcell);
+}
--- src/main/webapp/rno-gsm/gsm-ncell-relation/js/gsm-ncell-relation.js
+++ src/main/webapp/rno-gsm/gsm-ncell-relation/js/gsm-ncell-relation.js
@@ -1,135 +1,403 @@
 $(function () {
+    //tab选项卡
+    $("#tabs").tabs();//项目服务范围类别切换
+
+    // 设置导航标题
+    setNavTitle("navTitle");
 
     // 执行 laydate 实例 
-    laydate.render({elem: '#begUploadDate', value: new Date(new Date().getTime() - 7 * 86400000)});
-    laydate.render({elem: '#endUploadDate', value: new Date()});
-    laydate.render({elem: '#fileDate', value: new Date()});
-    laydate.render({elem: '#beginTestDate', value: new Date(new Date().getTime() - 7 * 86400000)});
-    laydate.render({elem: '#endTestDate', value: new Date()});
-
-    //执行 区域 实例 
-    $("#cell-data-province-menu").change(function () {
-        var cityId = parseInt($(this).find("option:checked").val());
-        $.getJSON("../../data/area.json", function (data) {
-            renderArea(data, cityId, "cell-data-city-menu");
-        })
+    var begDate = new Date(new Date().getTime() - 7 * 86400000);
+    var endDate = new Date();
+    var begUploadDate = laydate.render({//渲染开始时间选择
+        elem: '#begUploadDate', //通过id绑定html中插入的start
+        value: begDate,
+        max: endDate.toString(),//设置一个默认最大值
+        btns: ['clear', 'confirm'],
+        done: function (value, dates) {
+            endUploadDate.config.min = {
+                year: dates.year,
+                month: dates.month - 1, //关键
+                date: dates.date
+            };
+        }
     });
-
-    $("#cell-data-province-id").change(function () {
-        var provinceId = parseInt($(this).find("option:checked").val());
-        $.getJSON("../../data/area.json", function (data) {
-            renderArea(data, provinceId, "cell-data-city-id");
-        })
-    });
-
-    $("#cell-data-city-menu").change(function () {
-        var area1 = $("#cell-data-city-menu option:selected").text();
-        $("#area1").val(area1);
-        $("#area2").val(area1);
-    })
-
-
-    //初始化区域
-    $.ajax({
-        url: "../../data/area.json",
-        dataType: "json",
-        async: false,
-        success: function (data) {
-            renderArea(data, 0, "cell-data-province-id");
-            renderArea(data, 0, "cell-data-province-menu");
-            $("#cell-data-province-id").change();
-            $("#cell-data-province-menu").change();
+    var endUploadDate = laydate.render({//渲染结束时间选择
+        elem: '#endUploadDate',
+        value: endDate,
+        min: begDate.getFullYear()+"-"+(begDate.getMonth()+1)+"-"+begDate.getDate(),//设置min默认最小值
+        btns: ['clear', 'confirm'],
+        done: function (value, dates) {
+            begUploadDate.config.max = {
+                year: dates.year,
+                month: dates.month - 1,//关键
+                date: dates.date
+            }
         }
     });
 
-    $("#queryBtn").click(function () {
-        $('#queryResultTab').css("line-height", "12px");
-        $('#queryResultTab').DataTable( {
-            "ajax": "data/gsm-ncell-relation-import-data.json",
-            "columns": [
-                { "data": "city_name" },
-                { "data": "uploadTime" },
-                { "data": "fileName" },
-                { "data": "fileSize" },
-                { "data": "launchTime" },
-                { "data": "completeTime" },
-                { "data": "account" },
-                { "data": "fileStatus" , "render": function (data, type, row) {
-                    return '<a onclick="showDetail('+row["jobId"]+')">' + data + '</a>';
-                }}
-            ],
-            // "lengthChange": false,
-            // "ordering": false,
-            // "searching": false,
-            "language": {
-                url: '../../lib/datatables/1.10.16/i18n/Chinese.json'
-            }
-        } );
+    // 初始化区域联动
+    initAreaSelectors({selectors: ["provinceId", "cityId"]});
+    initAreaSelectors({selectors: ["provinceId2", "cityId2"]});
+
+    //显示隐藏导入窗口
+    $("#importTitleDiv").click(function () {
+        var flag = $("#importDiv").is(":hidden");//是否隐藏
+        if (flag) {
+            $(".importContent").show("fast");
+        } else {
+            $(".importContent").hide("fast");
+        }
     });
 
-    $("#queryCellBtn").click(function () {
-        $('#queryCellResultTab').css("line-height", "12px");
-        $('#queryCellResultTab').DataTable( {
-            "ajax": "data/gsm-ncell-relation-query-data.json",
-            "columns": [
-                { "data": "CITY_NAME" },
-                { "data": "MEA_DATE" },
-                { "data": "DATA_TYPE" },
-                { "data": "CELL_NUM" },
-                { "data": "ACCOUNT" },
-                { "data": "CREATE_TIME" }
-            ],
-            // "lengthChange": false,
-            // "ordering": false,
-            // "searching": false,
-            "language": {
-                url: '../../lib/datatables/1.10.16/i18n/Chinese.json'
-            }
-        } );
-    })
+    $("#queryImportDT").click(function () {
+        if($("#begUploadDate").val()===""||$("#endUploadDate").val()===""){
+            $("#info").css("background", "red");
+            showInfoInAndOut("info", "请选择正确的上传时间段");
+            return false;
+        }
+        $(".loading").show();
+    });
 
+    $("#queryDataTab").click(function () {
+        $(".loading").show();
+    });
+
+    //验证邻区关系查询条件
+    $("#queryBtn").click(function () {
+        var reg = /^[0-9]+.?[0-9]*$/;
+        var cellBsc = $("#cellBsc").val();
+        var ncellBsc = $("#ncellBsc").val();
+        $("#info").css("background", "red");
+        if (!reg.test(cellBsc) && cellBsc.trim() !== '') {
+            showInfoInAndOut("info", "主小区BSC值必须为数字");
+            return false;
+        }
+        if (cellBsc.length > 10 && cellBsc.trim() !== '') {
+            showInfoInAndOut("info", "主小区BSC值输入过长");
+            return false;
+        }
+        if (!reg.test(ncellBsc) && ncellBsc.trim() !== '') {
+            showInfoInAndOut("info", "邻小区BSC值必须为数字");
+            return false;
+        }
+        if (ncellBsc.length > 10 && ncellBsc.trim() !== '') {
+            showInfoInAndOut("info", "邻小区BSC值输入过长");
+            return false;
+        }
+        $(".loading").show();
+    });
+
+    // //AJAX 提交邻区关系查询条件表单
+    $("#conditionForm").ajaxForm({
+        url: "/api/gsm-ncell-relation/ncell-query",
+        success: showNcellRelationResult
+    });
+
+    //AJAX 提交邻区导入记录查询条件表单
+    $("#importQuery").ajaxForm({
+        url: "/api/gsm-ncell-relation/ncell-import-query",
+        success: showNcellImportResult
+    });
+
+    //AJAX 提交邻区关系数据查询条件表单
+    $("#searchNcellDtForm").ajaxForm({
+        url: "/api/gsm-ncell-relation/ncell-import-data-query",
+        success: showNcellImportDtResult
+    });
+
+    // AJAX 上传文件
+    var progress = $('.upload-progress');
+    var bar = $('.bar');
+    var percent = $('.percent');
+
+    //导入文件类型判断
+    $("#importBtn").click(function () {
+        var path = $("#file").val();
+        var cityId = $("#cityId").val();
+        $("#areaId").val(cityId);
+        var fileType = path.substring(path.lastIndexOf("."), path.length).toLowerCase();
+        if (fileType !== '.csv' && fileType !== '.zip') {
+            $("#info").css("background", "red");
+            showInfoInAndOut("info", "请上传csv或者zip格式的数据文件");
+            return false;
+        }
+    });
+
+    $("#file-upload-form").ajaxForm({
+        url: "/api/gsm-ncell-relation/upload-file",
+        beforeSend: function () {
+            progress.css("display", "block");
+            var percentVal = '0%';
+            bar.width(percentVal);
+            percent.html(percentVal);
+        },
+        uploadProgress: function (event, position, total, percentComplete) {
+            var percentVal = percentComplete + '%';
+            bar.width(percentVal);
+            percent.html(percentVal);
+        },
+        success: function () {
+            var percentVal = '100%';
+            bar.width(percentVal);
+            percent.html(percentVal);
+            $("#info").css("background", "green");
+            showInfoInAndOut("info", "文件导入成功！");
+            //AJAX 提交邻区导入记录查询条件表单
+            $("#importQuery").submit();
+        }
+    });
+
+    // 当上传文件域改变时，隐藏进度条
+    $("input[name='file']").change(function () {
+        progress.css("display", "none");
+    });
 });
 
-function showDetail(jobId) {
-    alert("jobId为："+jobId);
-    /*$('#queryDetailTab').css("line-height", "12px");
-    $('#queryDetailTab').DataTable( {
-        "ajax": "data/fas-import-detail-data.json",
-        "columns": [
-            { "data": "STAGE" },
-            { "data": "BEG_TIME" },
-            { "data": "END_TIME" },
-            { "data": "STATE" },
-            { "data": "ATT_MSG" }
-        ],
-        // "lengthChange": false,
-        // "ordering": false,
-        // "searching": false,
-        "language": {
-            url: '../../lib/datatables/1.10.16/i18n/Chinese.json'
-        }
-    } );
-    $("#importListDiv").css("display","none")
-    $("#reportDiv").css("display","block");*/
+// 显示邻区关系查询结果
+function showNcellRelationResult(data) {
+    $(".loading").css("display", "none");
+    if (data == '') {
+        $("#info").css("background", "red");
+        showInfoInAndOut('info', '没有符合条件的邻区关系');
+    }
+    $('#queryResultTab').DataTable().clear();
+
+    $('#queryResultTab').css("line-height", "12px")
+        .DataTable({
+            "data": data,
+            "columns": [
+                {"data": "cellName"},
+                {"data": "ncellName"},
+                {"data": "cellId"},
+                {"data": "cellEnName"},
+                {"data": "ncellId"},
+                {"data": "ncellEnName"},
+                {"data": "cellBsc"},
+                {"data": "ncellBsc"},
+                {"data": null}
+            ],
+            "columnDefs": [{
+                "render": function (data, type, row) {
+                    var id = row['id'];
+                    return "<a onclick=\"deleteCell('" + id + "')\">删除</a>";
+                },
+                "targets": -1,
+                "data": null
+            }
+            ],
+            "lengthChange": false,
+            "ordering": true,
+            "searching": false,
+            "destroy": true,
+            "language": {
+                url: '../../lib/datatables/1.10.16/i18n/Chinese.json'
+            }
+        });
 }
 
-// 渲染区域
-function renderArea(data, parentId, areaMenu) {
-    var arr = data.filter(function (v) {
-        return v.parentId === parentId;
-    });
-    if (arr.length > 0) {
-        var areaHtml = [];
-        $.each(arr, function (index) {
-            var area = arr[index];
-            areaHtml.push("<option value='" + area.id + "'>");
-            areaHtml.push(area.name + "</option>");
-        });
-        $("#" + areaMenu).html(areaHtml.join(""));
-    } else {
-        console.log("父ID为" + parentId + "时未找到任何下级区域。");
+//显示邻区关系导入记录查询结果
+function showNcellImportResult(data) {
+    $(".loading").css("display", "none");
+    if (data == '') {
+        $("#info").css("background", "red");
+        showInfoInAndOut('info', '没有符合条件的邻区关系导入记录');
     }
-    var area1 = $("#cell-data-city-menu option:selected").text();
-    $("#area1").val(area1);
-    $("#area2").val(area1);
+    $('#queryRecordResTab').DataTable().clear();
+    $('#queryRecordResTab').css("line-height", "12px")
+        .DataTable({
+            "data": data,
+            "columns": [
+                {"data": "areaName"},
+                {"data": "uploadTime"},
+                {"data": "filename"},
+                {"data": "fileSize"},
+                {"data": null},
+                {"data": null},
+                {"data": "createdUser"},
+                {"data": null}
+            ],
+            "columnDefs": [{
+                "render": function (data, type, row) {
+                    return new Date(parseInt(row['uploadTime'])).Format("yyyy-MM-dd hh:mm:ss");
+                },
+                "targets": 1,
+                "data": null
+            }, {
+                "render": function (data, type, row) {
+                    if (row['startTime'] === "" || row['startTime'] === null) {
+                        return "---";
+                    } else {
+                        return new Date(parseInt(row['startTime'])).Format("yyyy-MM-dd hh:mm:ss");
+                    }
+                },
+                "targets": 4,
+                "data": null
+            }, {
+                "render": function (data, type, row) {
+                    if (row['completeTime'] === "" || row['completeTime'] === null) {
+                        return "---";
+                    } else {
+                        return new Date(parseInt(row['completeTime'])).Format("yyyy-MM-dd hh:mm:ss");
+                    }
+                },
+                "targets": 5,
+                "data": null
+            }, {
+                "render": function (data, type, row) {
+                    switch (row['status']) {
+                        case "部分成功":
+                            return "<a style='color: red' onclick=\"showImportDetail('" + row['id'] + "')\">" + row['status'] + "</a>";
+                        case "全部失败":
+                            return "<a style='color: red' onclick=\"showImportDetail('" + row['id'] + "')\">" + row['status'] + "</a>";
+                        case "全部成功":
+                            return "<a onclick=\"showImportDetail('" + row['id'] + "')\">" + row['status'] + "</a>";
+                        case "正在处理":
+                            return "<a onclick=\"showImportDetail('" + row['id'] + "')\">" + row['status'] + "</a>";
+                        case "等待处理":
+                            return "<a onclick=\"showImportDetail('" + row['id'] + "')\">" + row['status'] + "</a>";
+                    }
+                },
+                "targets": -1,
+                "data": null
+            }
+            ],
+            "lengthChange": false,
+            "ordering": true,
+            "searching": false,
+            "destroy": true,
+            "language": {
+                url: '../../lib/datatables/1.10.16/i18n/Chinese.json'
+            }
+        });
 }
+
+//显示邻区关系数据查询结果
+function showNcellImportDtResult(data) {
+    $(".loading").css("display", "none");
+    if (data == '') {
+        $("#info").css("background", "red");
+        showInfoInAndOut('info', '没有符合条件的邻区数据记录');
+    }
+    $('#queryDataResultDT').DataTable().clear();
+    $('#queryDataResultDT').css("line-height", "12px")
+        .DataTable({
+            "data": data,
+            "columns": [
+                {"data": "areaName"},
+                {"data": "dataType"},
+                {"data": "filename"},
+                {"data": "recordCount"},
+                {"data": "createdDate"}
+            ],
+            "lengthChange": false,
+            "ordering": true,
+            "searching": false,
+            "destroy": true,
+            "language": {
+                url: '../../lib/datatables/1.10.16/i18n/Chinese.json'
+            }
+        });
+}
+
+function showInfoInAndOut(div, info) {
+    var divSet = $("#" + div);
+    divSet.html(info);
+    divSet.fadeIn(2000);
+    setTimeout("$('#" + div + "').fadeOut(2000)", 1000);
+}
+
+//删除邻区关系
+function deleteCell(id) {
+    $("#info").css("background", "red");
+    var r = confirm("删除该邻区关系？");
+    if (r === true) {
+        $.ajax({
+            url: '/api/gsm-ncell-relation/delete-by-id?id=' + id,
+            dataType: 'text',
+            type:'delete',
+            success: function () {
+                showInfoInAndOut("info", "删除邻区关系成功！");
+                $("#conditionForm").submit();
+            }, error: function (err) {
+                console.log(err);
+                showInfoInAndOut("info", "后台程序错误！");
+            }
+        })
+    }
+
+}
+
+//显示导入记录的状态的详情
+function showImportDetail(id) {
+    $.ajax({
+        url: '/api/gsm-ncell-relation/query-report',
+        dataType: 'text',
+        type:'post',
+        data: {id: id},
+        success:function(data){
+            $("#reportDiv").css("display", "block");
+            $("#listInfoDiv").css("display", "none");
+            $("#reportListTab").css("line-height", "12px")
+                .DataTable({
+                    "data": JSON.parse(data),
+                    "columns": [
+                        {"data": "stage"},
+                        {"data": "startTime"},
+                        {"data": "completeTime"},
+                        {"data": "status"},
+                        {"data": "message"}
+                    ],
+                    "lengthChange": false,
+                    "ordering": true,
+                    "searching": false,
+                    "destroy": true,
+                    "language": {
+                        url: '../../lib/datatables/1.10.16/i18n/Chinese.json'
+                    }
+                });
+        }, error: function (err) {
+            console.log(err);
+            $("#info").css("background", "red");
+            showInfoInAndOut("info", "后台程序错误！");
+        }
+    });
+}
+
+/**
+ * 从报告的详情返回列表页面
+ */
+function returnToImportList(){
+    $("#reportDiv").css("display","none");
+    $("#listInfoDiv").css("display","block");
+}
+
+/**
+ * 对Date的扩展，将 Date 转化为指定格式的String
+ *月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符，
+ *年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)
+ *例子：
+ *(new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423
+ *(new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18
+ * @title
+ * @param fmt
+ * @returns
+ */
+Date.prototype.Format = function (fmt) {
+    var o = {
+        "M+": this.getMonth() + 1,                 //月份
+        "d+": this.getDate(),                    //日
+        "h+": this.getHours(),                   //小时
+        "m+": this.getMinutes(),                 //分
+        "s+": this.getSeconds(),                 //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds()             //毫秒
+    };
+    if (/(y+)/.test(fmt))
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o) {
+        if (new RegExp("(" + k + ")").test(fmt)){
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        }
+    }
+    return fmt;
+};
