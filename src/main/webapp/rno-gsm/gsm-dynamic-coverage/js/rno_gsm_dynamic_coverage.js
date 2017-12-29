068bfee20f82fe0fb151c7eb38376e1bd2e9c5bd
萧顺智 <xiao.sz@hgicreate.com>
2017-12-29 09:52:00 +0000

    TASK #4425 GSM动态覆盖图功能增强 更新UI

--- src/main/webapp/rno-gsm/gsm-dynamic-coverage/js/rno_gsm_dynamic_coverage.js
+++ src/main/webapp/rno-gsm/gsm-dynamic-coverage/js/rno_gsm_dynamic_coverage.js
@@ -57,7 +57,7 @@ $(document).ready(function () {
         }
     });
     //打开查找小区窗口
-    $(".queryButton").click(function () {
+    $("#queryButton").click(function () {
         $("#searchDiv").toggle();
     });
     // 根据条件搜索小区
@@ -325,40 +325,16 @@ $(document).ready(function () {
     });
 
     $(".ol-unselectable.ol-control.layer-switcher").css("right", "350px").css("top", "0px");
-
     $("#areaId").change(function () {
         var lon = parseFloat($(this).find("option:checked").attr("data-lon"));
         var lat = parseFloat($(this).find("option:checked").attr("data-lat"));
         map.getView().setCenter([lon, lat]);
     });
+    $("#areaId").trigger("change");
 
-    $("#cityId").change(function () {
-        var cityId = parseInt($(this).find("option:checked").val());
-        $.getJSON("../../data/area.json", function (data) {
-            renderArea(data, cityId, "areaId", true);
-            $("#areaId").trigger("change");
-        })
-    });
-
-    $("#provinceId").change(function () {
-        var provinceId = parseInt($(this).find("option:checked").val());
-        $.getJSON("../../data/area.json", function (data) {
-            renderArea(data, provinceId, "cityId", false);
-            $("#cityId").trigger("change");
-        })
-    });
     //初始化区域
-    $.ajax({
-        url: "../../data/area.json",
-        dataType: "json",
-        async: false,
-        success: function (data) {
-            renderArea(data, 0, "provinceId");
-            renderArea(data, 440000, "cityId");
-            $("#provinceId").change();
-            $("#cityId").change();
-        }
-    });
+    initAreaSelectors({selectors: ["provinceId", "cityId", "areaId"], coord: true});
+
     $("#queryCellAreaId").click(function () {
         var btn = $("#queryCellAreaId");
         btn.val("loading");
@@ -857,29 +833,6 @@ function toConverVal(val) {
         return num.toFixed(4);
     } else {
         return num;
-    }
-}
-
-function renderArea(data, parentId, areaMenu, boolLonLat) {
-    var arr = data.filter(function (v) {
-        return v.parentId === parentId;
-    });
-    if (arr.length > 0) {
-        var areaHtml = [];
-        $.each(arr, function (index) {
-            var area = arr[index];
-
-            if (boolLonLat) {
-                areaHtml.push("<option value='" + area.id + "' data-lon='" + area.longitude + "' data-lat='" + area.latitude + "'>");
-            } else {
-                areaHtml.push("<option value='" + area.id + "'>");
-            }
-
-            areaHtml.push(area.name + "</option>");
-        });
-        $("#" + areaMenu).html(areaHtml.join(""));
-    } else {
-        console.log("父ID为" + parentId + "时未找到任何下级区域。");
     }
 }
 
