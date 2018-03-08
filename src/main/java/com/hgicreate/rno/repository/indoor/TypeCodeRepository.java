91c65ff7add9aaf5c1f1e79905e5cf4f74951b51
晁现杰 <chao.xj@hgicreate.com>
2018-03-08 09:42:04 +0800

    #5-为覆写方法必须Override注解

--- src/main/java/com/hgicreate/rno/repository/indoor/TypeCodeRepository.java
+++ src/main/java/com/hgicreate/rno/repository/indoor/TypeCodeRepository.java
@@ -6,8 +6,16 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * @author chao.xj
+ */
 @Repository
 public interface TypeCodeRepository extends JpaRepository<TypeCode,Long> {
 
+    /**
+     * 获取类型编码集合数据
+     * @return 类型编码集合数据
+     */
+    @Override
     List<TypeCode> findAll();
 }
